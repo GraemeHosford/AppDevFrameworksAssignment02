3eadb13399125b1ed64c82f725967bd4f7d74906
Graeme Hosford <graemehosford@gmail.com>
2019-11-06 18:09:55 +0000

    Finally fixed errors on startup

---
+++ src/main/java/graeme/hosford/rob/morgan/assignment2/Assignment02Application.java
@@ -0,0 +1,13 @@
+package graeme.hosford.rob.morgan.assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment02Application.class, args);
+    }
+
+}
--- src/main/java/graeme/hosford/rob/morgan/assignment2/controller/JobController.java
+++
@@ -1,9 +0,0 @@
-package graeme.hosford.rob.morgan.assignment2.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class JobController {
-
-
-}
